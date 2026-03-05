d48bb0528ccb03c13b4b9853a05435ed115afe80
glasmr <glasmannross@gmail.com>
2026-03-05 14:40:59 -0500

    First version NFA Builder completed, needs testing

--- src/engine_nfa/nfa_builder.rs
+++ src/engine_nfa/nfa_builder.rs
@@ -33,96 +33,145 @@ impl BuilderNFA {
             Ok(res) => res,
             Err(e) => return Err(format!("Error walking tree: {}", e)),
         };
-        unimplemented!()
+        let (start_state, end_state) = self.complete_nfa((start, end))?;
+        let states = self.nfa_vec.to_vec();
+        Ok(NFA{
+            states,
+            start_state, 
+            end_state
+        })
     }
 
     fn walk_tree(&mut self, ast: &NodeAST) -> Result<(usize, usize), String> {
         match ast {
             NodeAST::Literal(char) => {self.build_simple_nfa(CharToMatch::Literal(*char))}
+
             NodeAST::CharacterClass(char_class_type) => {self.build_simple_nfa(CharToMatch::CharacterClass(*char_class_type))}
+
             NodeAST::Any => {self.build_simple_nfa(CharToMatch::Any)}
+
             NodeAST::AnchorStart(next) => {unimplemented!()}
+
             NodeAST::AnchorEnd(next) => {unimplemented!()}
+
             NodeAST::CaptureGroup(next, grp_n) => {unimplemented!()}
+
             NodeAST::Quantifier(next, quantifier) => {
-                let state = self.walk_tree(next)?;
+                let nfa = self.walk_tree(next)?;
                 match quantifier {
-                    QuantifierType::ZeroOrMore => {}
-                    QuantifierType::OneOrMore => {}
-                    QuantifierType::ZeroOrOne => {}
-                    QuantifierType::Repetition(m, n) => {}
+                    QuantifierType::ZeroOrMore => {self.zero_or_more_nfa(nfa)}
+                    QuantifierType::OneOrMore => {self.one_or_more_nfa(nfa)}
+                    QuantifierType::ZeroOrOne => {self.zero_or_one_nfa(nfa)}
+                    QuantifierType::Repetition(m, n) => {self.repetition_nfa(nfa, *m, *n)}
                 }
-                unimplemented!()
             }
+
             NodeAST::Concatenation(next_a, next_b) => {
                 let state_a = self.walk_tree(next_a)?;
                 let state_b = self.walk_tree(next_b)?;
                 self.concat_nfa(state_a, state_b)
             }
-            NodeAST::Alternation(next_a, next_b) => {
-                let state_a = self.walk_tree(next_a)?;
-                let state_b = self.walk_tree(next_b)?;
 
-                self.alternate_nfa(state_a, state_b)
+            NodeAST::Alternation(next_a, next_b) => {
+                let nfa_a = self.walk_tree(next_a)?;
+                let nfa_b = self.walk_tree(next_b)?;
+
+                self.alternate_nfa(nfa_a, nfa_b)
             }
         }
         
     }
 
-    fn zero_or_more_nfa(&mut self, state: (usize, usize)) -> Result<(usize, usize), String> {
+    fn zero_or_more_nfa(&mut self, nfa: (usize, usize)) -> Result<(usize, usize), String> {
         let begin_state_index = self.nfa_vec.len();
         let end_state_index = begin_state_index + 1;
         //BEGIN state
         self.nfa_vec.push(State::new(
             StateType::Split,
-            Some((Transition::Epsilon(Some(state.0)), Some(Transition::Epsilon(Some(end_state_index)))))
+            Some((Transition::Epsilon(Some(nfa.0)), Some(Transition::Epsilon(Some(end_state_index)))))
         ));
         //END state
         self.nfa_vec.push(State::new(
             StateType::Literal,
             Some((Transition::Epsilon(None), None))
         ));
-        self.nfa_vec[state.1].change_state_type(StateType::Split);
-        self.nfa_vec[state.1].transition(
-            Some((Transition::Epsilon(Some(state.0)), Some(Transition::Epsilon(Some(end_state_index)))))
+        self.nfa_vec[nfa.1].change_state_type(StateType::Split);
+        self.nfa_vec[nfa.1].transition(
+            Some((Transition::Epsilon(Some(nfa.0)), Some(Transition::Epsilon(Some(end_state_index)))))
         );
 
         Ok((begin_state_index, end_state_index))
     }
 
-    fn one_or_more_nfa(&mut self, state: (usize, usize)) -> Result<(usize, usize), String> {
-        unimplemented!()
+    fn one_or_more_nfa(&mut self, nfa: (usize, usize)) -> Result<(usize, usize), String> {
+        self.nfa_vec[nfa.1].change_state_type(StateType::Split);
+        self.nfa_vec[nfa.1].connect_second_transition(nfa.0)?;
+        Ok(nfa)
     }
 
-    fn zero_or_one_nfa(&mut self, state: (usize, usize)) -> Result<(usize, usize), String> {
-        unimplemented!()
-    }
-
-    fn repetition_nfa(&mut self, state: (usize, usize), m: usize, n: Option<usize>) -> Result<(usize, usize), String> {
-        unimplemented!()
-    }
-    fn alternate_nfa(&mut self, state_a: (usize, usize), state_b: (usize, usize)) -> Result<(usize, usize), String> {
-        let state_index = self.nfa_vec.len();
-        let end_state_index = state_index + 1;
+    fn zero_or_one_nfa(&mut self, nfa: (usize, usize)) -> Result<(usize, usize), String> {
+        let nfa_start_state_index = self.nfa_vec.len();
+        let nfa_end_state_index = nfa_start_state_index + 1;
         self.nfa_vec.push(State::new(
             StateType::Split,
-            Some((Transition::Epsilon(Some(state_a.0)), Some(Transition::Epsilon(Some(state_b.0))))),
+            Some((Transition::Epsilon(Some(nfa.0)), Some(Transition::Epsilon(Some(nfa_end_state_index)))))
+        ));
+        self.nfa_vec.push(State::new(
+            StateType::Literal,
+            Some((Transition::Epsilon(None), None))
+        ));
+        self.nfa_vec[nfa.1].connect_first_transition(nfa_end_state_index)?;
+
+        Ok((nfa_start_state_index, nfa_end_state_index))
+    }
+
+    fn repetition_nfa(&mut self, nfa: (usize, usize), m: usize, n: Option<usize>) -> Result<(usize, usize), String> {
+        // In order for the last 'm' nfa to be made into a 'one or more'
+        // we cannot construct it in the loop because we will have
+        // no way to access it after, so we will remove it from the
+        // loop and handle it in the match
+        let mut m_nfa = nfa;
+        for _ in 0..m - 1 {
+            m_nfa = self.concat_nfa(m_nfa, nfa)?
+        }
+        match n {
+            Some(n) => {
+                m_nfa = self.concat_nfa(m_nfa, nfa)?; // last 'm' nfa handled here (when n exists)
+                let remaining = n - m;
+                let mut rem_nfa = nfa;
+                for _ in 0..remaining {
+                    rem_nfa = self.zero_or_one_nfa(rem_nfa)?;
+                }
+                Ok(self.concat_nfa(m_nfa, rem_nfa)?)
+            }
+            None => {
+                let last_nfa = self.one_or_more_nfa(nfa)?; //last 'm' nfa handled here (when no n)
+                Ok(self.concat_nfa(m_nfa, last_nfa)?)
+            }
+        }
+    }
+    fn alternate_nfa(&mut self, nfa_a: (usize, usize), nfa_b: (usize, usize)) -> Result<(usize, usize), String> {
+        let nfa_start_state_idx = self.nfa_vec.len();
+        let nfa_end_state_index = nfa_start_state_idx + 1;
+        self.nfa_vec.push(State::new(
+            StateType::Split,
+            Some((Transition::Epsilon(Some(nfa_a.0)), Some(Transition::Epsilon(Some(nfa_b.0))))),
         ));
         self.nfa_vec.push(State::new(
             StateType::Literal,
             Some((Transition::Epsilon(None), None))
         ));
 
-        self.nfa_vec[state_a.0].connect_next_state(end_state_index)?;
-        self.nfa_vec[state_b.0].connect_next_state(end_state_index)?;
+        self.nfa_vec[nfa_a.0].connect_first_transition(nfa_end_state_index)?;
+        self.nfa_vec[nfa_b.0].connect_first_transition(nfa_end_state_index)?;
 
-        Ok((state_index, end_state_index))
+        Ok((nfa_start_state_idx, nfa_end_state_index))
     }
     
     /// Connects the dangling ends of state A (first) to the input of state B (second)
-    fn concat_nfa(&mut self, state_a: (usize, usize), state_b: (usize, usize)) -> Result<(usize, usize), String> {
-        self.nfa_vec[state_a.0].connect_next_state(state_b.0)?;
-        Ok((state_a.0, state_b.1))
+    fn concat_nfa(&mut self, nfa_a: (usize, usize), nfa_b: (usize, usize)) -> Result<(usize, usize), String> {
+        self.nfa_vec[nfa_a.0].connect_first_transition(nfa_b.0)?;
+        Ok((nfa_a.0, nfa_b.1))
     }
 
     fn build_simple_nfa(&mut self, c: CharToMatch) -> Result<(usize, usize), String> {
@@ -141,6 +190,13 @@ impl BuilderNFA {
 
     // Adds the final match state to the nfa and returns the index
     // of the start, and of the end.
-    fn complete_nfa(&mut self, nfa: (usize, Vec<usize>)) -> Result<(usize, usize), String> {unimplemented!()}
+    fn complete_nfa(&mut self, nfa: (usize, usize)) -> Result<(usize, usize), String> {
+        let matched_state_index = self.nfa_vec.len();
+        self.nfa_vec.push(State::new(
+            StateType::Match,
+            None
+        ));
+        Ok(self.concat_nfa(nfa, (matched_state_index, matched_state_index))?)
+    }
     
 }
