87087aa3a94613fbda4a65a5183f6629b27c3118
glasmr <glasmannross@gmail.com>
2026-04-03 12:25:49 -0400

    No major changes

--- src/matcher.rs
+++ src/matcher.rs
@@ -1,15 +1,15 @@
 use crate::types::nfa_types::CharToMatch;
 use crate::types::token_types::{CharClassType, CharSetType};
 
-pub fn matcher(c: char, m: CharToMatch) -> bool {
+pub fn matcher(c: char, m: &CharToMatch) -> bool {
     match m {
-        CharToMatch::Literal(ch) => c == ch,
+        CharToMatch::Literal(ch) => c == *ch,
         CharToMatch::Any => true,
         CharToMatch::CharacterClass(char_class) => {match_character_class(c, char_class)}
         CharToMatch::CharacterSet(negated, char_set) => {
             let mut result: bool = false;
             for c_class in char_set.iter() {
-                if match_character_class(c, *c_class) {result = true; break;}
+                if match_character_class(c, c_class) {result = true; break;}
             }
             if negated.eq(&CharSetType::Negated) {
                 return !result;
@@ -19,16 +19,16 @@ pub fn matcher(c: char, m: CharToMatch) -> bool {
     }
 }
 
-fn match_character_class(c: char, char_class: CharClassType) -> bool {
+fn match_character_class(c: char, char_class: &CharClassType) -> bool {
     match char_class {
         CharClassType::Any => true,
-        CharClassType::Literal(ch) => c == ch,
+        CharClassType::Literal(ch) => c == *ch,
         CharClassType::Word => {c.is_ascii_alphanumeric() || c == '_'}
         CharClassType::NonWord => {!c.is_ascii_alphanumeric() && c != '_'}
         CharClassType::Digit => {c.is_ascii_digit()}
         CharClassType::NonDigit => {!c.is_ascii_digit()}
         CharClassType::Whitespace => {c.is_ascii_whitespace()}
         CharClassType::NonWhitespace => {!c.is_ascii_whitespace()}
-        CharClassType::Range(first, second) => {c >= first && c <= second}
+        CharClassType::Range(first, second) => {c >= *first && c <= *second}
     }
 }
